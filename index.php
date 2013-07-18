7b67bd265c2f594dc82c682342c0056d4ed474c8
Willian <Willian@Willian-PC>
2013-07-17 22:42:47 -0300

    index.php 		      - menu lateral diminuido e codigo organizado para melhor vizualização estilos.php 		      - retirei codigos inuteis que sobraram do tecteruel e estavam poluindo o codigo atoa cadastro.php 	      - formulario totalmente pronto frm_incluirProduto 	      - retirei oque era desnecessario e coloquei oque faltava estilo_conteudo.php     - arrumei espaçamento dos produtos na home listarCarrinho.php	      - apenas mudei <h2> para <h3> para arrumar os espaçamentos do estilo_conteudo.php

--- index.php
+++ index.php
@@ -7,55 +7,65 @@
 </head>
 <body>
       
-  <section id="principal">
-  
+<section id="principal">
+      
+      
+      
+      
+      
     <section id="cabecalho">
 	 <header> 
 	
-             <a href="index.php"> <img src="imagens/topo/logo_comiccents.png" id="logo"/> </a>
-             
-                <label id="busca">
+            <a href="index.php"> <img src="imagens/topo/logo_comiccents.png" id="logo"/> </a>
+            
+            
+            <label id="busca">
                 Buscar:
                 <input type="text" placeholder="Busque aqui">
                 <img src="imagens/botoes_setas/botao.png">
-                </label>  
+                
+            </label>  
                 <br />
 	 
+                
             <nav>
 		<h1 id="menu_topo">
-		 <a class="link_titulo" href="listarCarrinho.php"   target="quadro">  Home  </a>  &nbsp &nbsp | &nbsp &nbsp
-		 <a class="link_titulo" href="listarCarrinho.php"   target="quadro">  Marvel  </a>  &nbsp &nbsp | &nbsp &nbsp 
-		 <a class="link_titulo" href="listarCarrinho.php"   target="quadro">  DC  </a>  &nbsp &nbsp | &nbsp &nbsp 
-		 <a class="link_titulo" href="listarCarrinho.php"   target="quadro">  Image  </a>  &nbsp &nbsp | &nbsp &nbsp 
+		 <a class="link_titulo" href="listarCarrinho.php"   target="quadro">  Home    </a> &nbsp; &nbsp; | &nbsp; &nbsp;
+		 <a class="link_titulo" href="listarCarrinho.php"   target="quadro">  Marvel  </a> &nbsp; &nbsp; | &nbsp; &nbsp; 
+		 <a class="link_titulo" href="listarCarrinho.php"   target="quadro">  DC      </a> &nbsp; &nbsp; | &nbsp; &nbsp; 
+		 <a class="link_titulo" href="listarCarrinho.php"   target="quadro">  Image   </a> &nbsp; &nbsp; | &nbsp; &nbsp; 
 		 <a class="link_titulo" href="listarCarrinho.php"   target="quadro">  Mang&aacute;s  </a>
 		</h1>
             </nav>
+                
 	 </header>
     </section> 	
 	
 	
 	
     
+      
+      
+      
+      
+      
+      
     <section id="menu_esquerdo">
-	 <nav id="lateral">
-	 
-	 <br />
-	 
-                <form action="login.php" name="login" method="post" id="formulario">
+        <nav id="lateral"> <br /> 
+         
+            
+            <form action="login.php" name="login" method="post" id="formulario">
                 
-                    Email: &nbsp&nbsp&nbsp <input type="text" name="usuario" autofocus="autofocus" required="required" size="15"/>
+                    E-mail: &nbsp; <input type="text" name="usuario" autofocus="autofocus" required="required" size="15"/>
                     <br /><br />
-                    Senha: &nbsp <input type="password" name="senha" size="15" />
+                    Senha: &nbsp; <input type="password" name="senha" size="15" />
                     <br /><br />
-                    <input type="submit" value="Entrar" /> &nbsp &nbsp 
+                    <input type="submit" value="Entrar" /> &nbsp; &nbsp;
                     <a href="cadastro.php" target="quadro" id="cadastro"> Cadastre-se </a>
 				   
-                </form>
+            </form>
 			
-			
-			
-	 
-	 
+ 
             <h1> Destaques </h1>
               <ul class="nav">
                    <li> <a href="listarCarrinho.php" target="quadro"> Homem-aranha   </a> </li>
@@ -71,10 +81,6 @@
                    <li> <a href="listarCarrinho.php" target="quadro"> Flash </a> </li>
                    <li> <a href="listarCarrinho.php" target="quadro"> Lanterna Verde </a> </li>
                    <li> <a href="listarCarrinho.php" target="quadro"> Deadpool </a> </li>
-                   <li> <a href="listarCarrinho.php" target="quadro"> Motoqueiro Fantasma </a> </li>
-                   <li> <a href="listarCarrinho.php" target="quadro"> Quarteto Fant&aacute;stico </a> </li>
-                   <li> <a href="listarCarrinho.php" target="quadro"> Thor </a> </li>
-                   <li> <a href="listarCarrinho.php" target="quadro"> Ms. Marvel </a> </li>
                    <li> <a href="listarCarrinho.php" target="quadro"> Dragon Ball </a> </li>
                    <li> <a href="listarCarrinho.php" target="quadro"> One Piece </a> </li>
                    <li> <a href="listarCarrinho.php" target="quadro"> Death Note </a> </li>
@@ -86,32 +92,48 @@
 
   
 	 </nav>
-  	</section>	
-	
-	
-    <section id="conteudo">
-       	  <iframe action="listarCarrinho.php" id="quadro" >
-     	
-           </iframe>
-	  </section>
+    </section>	
 
       
-	<section id="rodape">
+	
+      
+      
+      
+      
+      
+      
+      
+    <section id="conteudo">
+
+                <iframe src="listarCarrinho.php" name="quadro" frameborder="" id="frame"></iframe>	
+	  
+    </section>
+
+      
+      
+      
+      
+      
+      
+      
+      
+      
+    <section id="rodape">
 	  <footer>
 	    <center>
-		<a href="">Projeto</a>      &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp
-		<a href="">Equipe</a>       &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp
-		<a href="">Mapa do Site</a> &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp
+		<a href="">Projeto</a>      &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+		<a href="">Equipe</a>       &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+		<a href="">Mapa do Site</a> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
 		<a href="">Fale conosco</a>
 		</center>
 		
 		<br />
 		
 	    <center>TCC - Projeto acad&ecirc;mico - INI3CM - 2013<br/>
-	    <center>Rua Calopsita, 11 - Engenho Novo - Barueri - CEP:06460-112 - 2013</center>
+	    <center>Rua Calopsita, 11 - Engenho Novo - Barueri - CEP: 06460-112 - 2013</center>
 		
 	  </footer>
-	</section>
+    </section>
 	
 	
 	
@@ -119,6 +141,6 @@
 	
 	
 	 
-   </section>
+</section>
 </body>
 </html>
