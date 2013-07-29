2bce333ba40934709ef30c145a39951ba90c7ec9
Willian <Willian@Willian-PC>
2013-07-28 22:37:17 -0300

    sistema de busca feito comentario feito por vinicius castro

--- conexao/conecta.php
+++ conexao/conecta.php
@@ -1,10 +1,8 @@
-<?php
-$host = "localhost";
-$database = "comiccentsdb";
-$username = "root";
+<?php 
+$host = "localhost"; 
+$database = "comiccentsdb"; 
+$username = "root"; 
 $password = "";
 
-$connection = mysql_connect($host,$username,$password);
- mysql_select_db($database,$connection);
-
+$connection = mysql_connect($host,$username,$password)or die(mysql_error());; mysql_select_db($database,$connection)or die(mysql_error());;
 ?>
