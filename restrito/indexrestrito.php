90dbd84f6f16671843b61ec1af78c70ae8da5f9f
Willian <Willian@Willian-PC>
2013-08-04 22:02:33 -0300

    frame retirado sistema de busca aprimorado paginas dos menus acressentadas

--- restrito/indexrestrito.php
+++ restrito/indexrestrito.php
@@ -3,6 +3,7 @@
 <head>
 <title>Comic Cents</title>
 <link rel="stylesheet" type="text/css" href="../estilos.css" />
+<meta http-equiv="content-type" content="text/html;charset=utf-8" />
 
 </head>
 <body>
@@ -14,20 +15,24 @@
 	
              <a href="indexrestrito.php"> <img src="../imagens/topo/logo_comiccents.png" id="logo"/> </a>
              
-                <label id="busca">
-                Buscar:
-                <input type="text" placeholder="Busque aqui">
-                <img src="../imagens/botoes_setas/botao.png">
-                </label>  
+            <label id="campo_busca">
+                <form name="searchform" method="post" action="../exibir_busca.php">
+                <input type="text" name="buscar" placeholder="Busque aqui" />
+                <input type="submit" value="Buscar" />
+                </form>
+            </label>  
                 <br />
 	 
             <nav>
 		<h1 id="menu_topo">
-		 <a class="link_titulo" href="../listarCarrinho.php"   target="quadro">  Home  </a>  &nbsp &nbsp | &nbsp &nbsp
-		 <a class="link_titulo" href="../listarCarrinho.php"   target="quadro">  Marvel  </a>  &nbsp &nbsp | &nbsp &nbsp 
-		 <a class="link_titulo" href="../listarCarrinho.php"   target="quadro">  DC  </a>  &nbsp &nbsp | &nbsp &nbsp 
-		 <a class="link_titulo" href="../listarCarrinho.php"   target="quadro">  Image  </a>  &nbsp &nbsp | &nbsp &nbsp 
-		 <a class="link_titulo" href="../listarCarrinho.php"   target="quadro">  Mang&aacute;s  </a>
+                 <a class="link_titulo" href="index.php">  Home    </a> &nbsp; &nbsp; | &nbsp; &nbsp;
+		 <a class="link_titulo" href="marvel.php"> Marvel  </a> &nbsp; &nbsp; | &nbsp; &nbsp; 
+		 <a class="link_titulo" href="dc.php">     DC      </a> &nbsp; &nbsp; | &nbsp; &nbsp; 
+		 <a class="link_titulo" href="image.php">  Image   </a> &nbsp; &nbsp; | &nbsp; &nbsp;
+		 <a class="link_titulo" href="manga.php">  Mangás  </a> &nbsp; &nbsp; | &nbsp; &nbsp;
+                 <a class="link_titulo" href="hentai.php"> Hentai  </a> &nbsp; &nbsp; | &nbsp; &nbsp;
+                 <a class="link_titulo" href="gibi.php">   Gibis   </a> &nbsp; &nbsp; | &nbsp; &nbsp;
+                 <a class="link_titulo" href="album.php">  Álbuns de Luxo  </a>
 		</h1>
             </nav>
 	 </header>
@@ -41,7 +46,7 @@
 	 
 	 <br />
 	 
-                <form action="login.php" name="login" method="post" id="formulario">
+                <form action="../login.php" name="login" method="post" id="formulario">
                 
                  <a href="exibirInfo.php" id="cadastro"> Minhas Informa&ccedil;&otilde;es </a><br/><br/>
                 
@@ -53,62 +58,99 @@
 			
 	 
 	 
+           
             <h1> Destaques </h1>
+             <form name="searchform" method="post" action="../busca_menu.php" target="quadro">
               <ul class="nav">
-                   <li> <a href="../listarCarrinho.php" target="quadro"> Homem-aranha   </a> </li>
-                   <li> <a href="../listarCarrinho.php" target="quadro"> Batman 		   </a> </li>
-                   <li> <a href="../listarCarrinho.php" target="quadro"> Super-man      </a> </li>
-        	   <li> <a href="../listarCarrinho.php" target="quadro"> Liga da Justi&ccedil;a </a> </li>
-        	   <li> <a href="../listarCarrinho.php" target="quadro"> Vingadores </a> </li>
-                   <li> <a href="../listarCarrinho.php" target="quadro"> Homem de Ferro </a> </li> 
-                   <li> <a href="../listarCarrinho.php" target="quadro"> Capit&atilde;o Am&eacute;rica </a> </li>
-        	   <li> <a href="../listarCarrinho.php" target="quadro"> Wolverine </a> </li>
-        	   <li> <a href="../listarCarrinho.php" target="quadro"> X-men </a> </li>
-                   <li> <a href="../listarCarrinho.php" target="quadro"> Hulk </a> </li>
-                   <li> <a href="../listarCarrinho.php" target="quadro"> Flash </a> </li>
-                   <li> <a href="../listarCarrinho.php" target="quadro"> Lanterna Verde </a> </li>
-                   <li> <a href="../listarCarrinho.php" target="quadro"> Deadpool </a> </li>
-                   <li> <a href="../listarCarrinho.php" target="quadro"> Motoqueiro Fantasma </a> </li>
-                   <li> <a href="../listarCarrinho.php" target="quadro"> Quarteto Fant&aacute;stico </a> </li>
-                   <li> <a href="../listarCarrinho.php" target="quadro"> Thor </a> </li>
-                   <li> <a href="../listarCarrinho.php" target="quadro"> Ms. Marvel </a> </li>
-                   <li> <a href="../listarCarrinho.php" target="quadro"> Dragon Ball </a> </li>
-                   <li> <a href="../listarCarrinho.php" target="quadro"> One Piece </a> </li>
-                   <li> <a href="../listarCarrinho.php" target="quadro"> Death Note </a> </li>
-                   <li> <a href="../listarCarrinho.php" target="quadro"> Naruto </a> </li>
-                   <li> <a href="../listarCarrinho.php" target="quadro"> Bleach </a> </li>
-                   <li> <a href="../listarCarrinho.php" target="quadro"> Spaw </a> </li>
-                   <li> <a href="../listarCarrinho.php" target="quadro"> Turma da M&ocirc;nica </a> </li>
+                   <li> <a href="../destaques/produto_homemaranha.php">Homem-aranha</a> </li>
+                   <li> <a href="../destaques/produto_batman.php">Batman</a> </li>
+                   <li> <a href="../destaques/produto_superman.php">Super-man</a> </li>
+        	   <li> <a href="../destaques/produto_liga.php">Liga da Justi&ccedil;a</a> </li>
+        	   <li> <a href="../destaques/produto_vingadores.php">Vingadores</a> </li>
+                   <li> <a href="../destaques/produto_homemdeferro.php">Homem de Ferro</a> </li> 
+                   <li> <a href="../destaques/produto_capitaoamerica.php">Capit&atilde;o Am&eacute;rica</a> </li>
+        	   <li> <a href="../destaques/produto_wolverine.php">Wolverine</a> </li>
+        	   <li> <a href="../destaques/produto_xmen.php">X-men</a> </li>
+                   <li> <a href="../destaques/produto_hulk.php">Hulk</a> </li>
+                   <li> <a href="../destaques/produto_flash.php">Flash</a> </li>
+                   <li> <a href="../destaques/produto_lanternaverde.php">Lanterna Verde</a> </li>
+                   <li> <a href="../destaques/produto_deadpool.php">Deadpool</a> </li>
+                   <li> <a href="../destaques/produto_dragonball.php">Dragon Ball</a> </li>
+                   <li> <a href="../destaques/produto_onepiece.php">One Piece</a> </li>
+                   <li> <a href="../destaques/produto_deathnote.php">Death Note</a> </li>
+                   <li> <a href="../destaques/produto_naruto.php">Naruto</a> </li>
+                   <li> <a href="../destaques/produto_bleach.php">Bleach</a> </li>
+                   <li> <a href="../destaques/produto_spawn.php">Spaw</a> </li>
+                   <li> <a href="../destaques/produto_monica.php">Turma da M&ocirc;nica</a> </li>
               </ul>
-
+             </form>
   
 	 </nav>
-  	</section>	
-	
-	
-    <section id="conteudo">
-       	  <iframe action="../listarCarrinho.php" id="quadro" >
-     	
-           </iframe>
-	  </section>
+    </section>	
 
       
-	<section id="rodape">
+	
+      
+      
+      
+      
+      
+      
+      
+    <section id="conteudo">
+
+                <?php
+
+include_once'../conexao/conecta.php';
+
+$sql = "SELECT * FROM produtos";
+$qr = mysql_query($sql) or die (mysql_error());
+
+while($ln = mysql_fetch_assoc($qr)){
+    
+echo '<link rel="stylesheet" type="text/css" href="../estilo_conteudo.css" />';
+ 
+echo '<div><h3>'.$ln['NOME_PRODUTO'].'</h3> <br />';
+
+echo '<img src="../imagens/'.$ln['IMAGEMURL_PRODUTO'].'" /> <br />';
+
+echo 'Pre&ccedil;o: R$'.$ln['PRECOVENDA_PRODUTO'].'<br />';
+
+echo '<a href="../carrinho.php?acao=add&COD_PRODUTO='.$ln['COD_PRODUTO'].'" id="comprar">Comprar</a></div>';
+
+
+
+}
+
+?>	
+	  
+    </section>
+
+      
+      
+      
+      
+      
+      
+      
+      
+      
+    <section id="rodape">
 	  <footer>
 	    <center>
-		<a href="">Projeto</a>      &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp
-		<a href="">Equipe</a>       &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp
-		<a href="">Mapa do Site</a> &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp
+		<a href="">Projeto</a>      &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+		<a href="">Equipe</a>       &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+		<a href="">Mapa do Site</a> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
 		<a href="">Fale conosco</a>
 		</center>
 		
 		<br />
 		
 	    <center>TCC - Projeto acad&ecirc;mico - INI3CM - 2013<br/>
-	    <center>Rua Calopsita, 11 - Engenho Novo - Barueri - CEP:06460-112 - 2013</center>
+	    <center>Rua Calopsita, 11 - Engenho Novo - Barueri - CEP: 06460-112 - 2013</center>
 		
 	  </footer>
-	</section>
+    </section>
 	
 	
 	
@@ -116,6 +158,6 @@
 	
 	
 	 
-   </section>
+</section>
 </body>
 </html>
